fc5ac1a17441c95d4fb4fcfeb4e629e18d0b10dd
ActivityTime <a1161327317@qq.com>
2019-03-06 23:16:21 +0800

    rest格式修改完毕

--- AnimalBracelet/BackData/src/main/java/neau/cekong/controller/InserDataController.java
+++ AnimalBracelet/BackData/src/main/java/neau/cekong/controller/InserDataController.java
@@ -19,39 +19,62 @@ import neau.cekong.service.InsertNewDataByDidService;
 public class InserDataController {
 	@Resource
 	private InsertNewDataByDidService insertNewDataByDidService;
-	
+
 	@RequestMapping("wendu/{proId}/{wendu}")
 	@ResponseBody
-	public String save(TableWendu record,@PathVariable Long proId,@PathVariable Double wendu){	
+	public String save(TableWendu record, @PathVariable Long proId, @PathVariable Double wendu) {
 		record.setProductId(proId);
 		record.setWendu(wendu);
 		record.setTime(new Date());
 		System.out.println("Controller" + proId + " " + wendu);
-		insertNewDataByDidService.insertInto(record);	
+		insertNewDataByDidService.insertInto(record);
 		return "OK";
 	}
-	
+
 	@RequestMapping("wendu")
 	@ResponseBody
-	public String save(TableWendu record){
+	public String save(TableWendu record) {
 		record.setTime(new Date());
-		insertNewDataByDidService.insertInto(record);	
+		insertNewDataByDidService.insertInto(record);
 		return "OK";
 	}
-	
+
 	@RequestMapping("zishi")
 	@ResponseBody
-	public String save(TableZishi record){
+	public String save(TableZishi record) {
 		record.setTime(new Date());
-		insertNewDataByDidService.insertInto(record);	
+		insertNewDataByDidService.insertInto(record);
 		return "OK";
 	}
-	
+
+	@RequestMapping("zishi/{proId}/{zhan}/{ce}/{tang}")
+	@ResponseBody
+	public String save(TableZishi record, @PathVariable Long proId, @PathVariable Double zhan, @PathVariable Double ce,
+			@PathVariable Double tang) {
+		record.setTime(new Date());
+		record.setProductId(proId);
+		record.setZhan(zhan);
+		record.setCe(ce);
+		record.setTang(tang);
+		insertNewDataByDidService.insertInto(record);
+		return "OK";
+	}
+
 	@RequestMapping("bushu")
 	@ResponseBody
-	public String save(TableBushu record){	
+	public String save(TableBushu record) {
 		record.setTime(new Date());
-		insertNewDataByDidService.insertInto(record);	
+		insertNewDataByDidService.insertInto(record);
+		return "OK";
+	}
+
+	@RequestMapping("bushu/{proId}/{bushu}")
+	@ResponseBody
+	public String save(TableBushu record, @PathVariable Long proId, @PathVariable Long bushu) {
+		record.setProductId(proId);
+		record.setBushu(bushu);
+		record.setTime(new Date());
+		insertNewDataByDidService.insertInto(record);
 		return "OK";
 	}
 }
